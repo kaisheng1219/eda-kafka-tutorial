8a17ff16b7d33df11b44da8e2073ac25226036b9
Kai Sheng <76138041+kaisheng1219@users.noreply.github.com>
2024-04-25 14:48:43 +0800

    :bug: bug: fix jsonKafkaProducer missing annotation

--- src/main/java/dev/kafkademo/kafka/JsonKafkaProducer.java
+++ src/main/java/dev/kafkademo/kafka/JsonKafkaProducer.java
@@ -7,7 +7,9 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class JsonKafkaProducer {
     private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaProducer.class);
     private final KafkaTemplate<String, User> kafkaTemplate;
